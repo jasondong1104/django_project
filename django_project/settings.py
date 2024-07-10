ba287c4b632bd1b2fef1bc7e86da19b4890103fe
d30054343 <1441141917jason@gmail.com>
2024-07-10 16:55:33 +0800

    Support the service of static files and media files.

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,11 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+TEMPLATES_DIR = Path.joinpath(BASE_DIR, 'templates')
+STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
+MEDIA_DIR = Path.joinpath(BASE_DIR, 'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-7+!h6m51d1u3ak^8vy#j4+o=p894k+-0u@ovmfju6s)via@j-m"
 
@@ -56,7 +54,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR,],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [STATIC_DIR,]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Rango app say hello to you!<br/><a href="/rango/about/">About</a>')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # return HttpResponse('Rango app say hello to you!<br/><a href="/rango/about/">About</a>')
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse('This is a about info.<a href="/rango/">Index</a>')
